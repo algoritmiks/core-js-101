b943d466559c83e0c065440fe50d3e26e3045b33
Aleksey Osipov <alexey11rus@gmail.com>
2020-05-16 14:37:46 +0300

    feat: add some tasks

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -26,9 +26,18 @@
  *  20 => 'Buzz'
  *  21 => 'Fizz'
  *
- */
-function getFizzBuzz(/* num */) {
-  throw new Error('Not implemented');
+*/
+function getFizzBuzz(num) {
+  if (num % 3 === 0 && num % 5 === 0) {
+    return 'FizzBuzz';
+  }
+  if (num % 3 === 0) {
+    return 'Fizz';
+  }
+  if (num % 5 === 0) {
+    return 'Buzz';
+  }
+  return num;
 }
 
 
@@ -43,8 +52,11 @@ function getFizzBuzz(/* num */) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  if (n === 0) {
+    return 1;
+  }
+  return n * getFactorial(n - 1);
 }
 
 
